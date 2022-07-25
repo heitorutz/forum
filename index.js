d89d7b0f028a821bb8d6e007e3dafd96da4fa26d
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-24 23:00:07 -0300

    partials

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.status(200).render('index');
 });
 
+app.get('/ask', (req, res) => {
+    res.status(200).render('ask');
+});
+
 app.listen(3000, (err) => {
     if (err) console.log(err);
     console.log('Server running!');
