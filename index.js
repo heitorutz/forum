f97d4ae49f12edad993c4671c0d8904110877d6b
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-24 22:15:09 -0300

    bootstrap

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:name?', (req, res) => {
-    const userName = req.params.name;
-    const show = true;
-
-    res.status(200).render('index', {
-        name: userName,
-        show
-    });
+app.get('/', (req, res) => {
+    res.status(200).render('index');
 });
 
 app.listen(3000, (err) => {
