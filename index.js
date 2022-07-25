7701be6e16ffe012faaca95684e39bbb913c37b8
Heitor Urbanetz <jhty123453@gmail.com>
2022-07-24 23:27:17 -0300

    post method form ask

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.status(200).render('index');
 });
 
 app.get('/ask', (req, res) => {
+    res.status(200).render('ask');
+});
+
+app.post('/ask', (req, res) => {
+    const title = req.body.title;
+    const desc = req.body.desc;
+    console.log(title, desc);
     res.status(200).render('ask');
 });
 
